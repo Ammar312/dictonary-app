6caa093c1c27ab35528ff9f96917b143bbd6e4da
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2023-10-22 15:38:57 +0500

    error handle

--- dictonary-app/src/Dict.jsx
+++ dictonary-app/src/Dict.jsx
@@ -8,7 +8,6 @@ const Dict = () => {
     const word = document.querySelector("#word").value.toLowerCase();
     e.preventDefault();
     if (word === "" || isNaN(word) === false) {
-      alert("404");
       document.querySelector(".container").style.display = "none";
       document.querySelector(".error").style.display = "block";
     } else {
@@ -16,19 +15,17 @@ const Dict = () => {
         .get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
         .then(function (response) {
           // handle success
-          console.log(response.data);
-          // console.log(response.data[0].meanings[0].definitions[0].definition);
           const wordName = response.data[0]?.word;
           let audio;
           if (
-            response.data[0].phonetics[0].audio === "" &&
-            response.data[0].phonetics[1].audio === ""
+            response.data[0]?.phonetics[0]?.audio === "" &&
+            response.data[0]?.phonetics[1]?.audio === ""
           ) {
-            audio = response.data[0].phonetics[2].audio;
-          } else if (response.data[0].phonetics[0].audio === "") {
-            audio = response.data[0].phonetics[1].audio;
+            audio = response.data[0]?.phonetics[2]?.audio;
+          } else if (response.data[0]?.phonetics[0]?.audio === "") {
+            audio = response.data[0]?.phonetics[1]?.audio;
           } else {
-            audio = response.data[0].phonetics[0].audio;
+            audio = response.data[0]?.phonetics[0]?.audio;
           }
           // const audio1 = response.data[0].phonetics[1].audio;
 
@@ -39,7 +36,7 @@ const Dict = () => {
             response.data[0]?.meanings[0]?.definitions[1]?.definition;
           const synonyms = response.data[0]?.meanings[0]?.synonyms;
           const synonyms1 = response.data[0]?.meanings[1]?.synonyms;
-          console.log(synonyms);
+
           const partOfSpeech1 = response.data[0]?.meanings[1]?.partOfSpeech;
           const meaning1Verb =
             response.data[0]?.meanings[1]?.definitions[0]?.definition;
@@ -62,7 +59,7 @@ const Dict = () => {
         .catch(function (error) {
           // handle error
           console.log(error.message);
-          alert(error.message);
+
           // document.querySelector(".container").style.backgroundColor = "blue";
         });
       document.querySelector(".error").style.display = "none";
@@ -88,16 +85,18 @@ const Dict = () => {
       <section className="error">404</section>
       <section className="container">
         <div className="wordName">
-          <div className="iconDiv">
-            <i
-              className="fa-solid fa-volume-high audioIcon"
-              onClick={() => {
-                document.querySelector("#audio").play();
-              }}
-            >
-              <audio src={dict.audio} id="audio"></audio>
-            </i>
-          </div>
+          {audio && (
+            <div className="iconDiv">
+              <i
+                className="fa-solid fa-volume-high audioIcon"
+                onClick={() => {
+                  document.querySelector("#audio").play();
+                }}
+              >
+                <audio src={dict.audio} id="audio"></audio>
+              </i>
+            </div>
+          )}
           <span>{dict.wordName}</span>
         </div>
         <div className="partOfSpeech">
@@ -105,34 +104,40 @@ const Dict = () => {
         </div>
         <div className="meaningSection">
           <article>1.&nbsp; {dict.meaning1}</article>
-          <article className="synonymsDiv">
-            <span>Similar:</span>
-            {dict.synonyms?.map((syn, index) => {
-              return (
-                <div className="synonym" key={index}>
-                  {syn}
-                </div>
-              );
-            })}
-          </article>
-          <article>2. &nbsp;{dict.meaning2}</article>
+          {dict.synonyms && (
+            <article className="synonymsDiv">
+              <span>Similar:</span>
+              {dict.synonyms?.map((syn, index) => {
+                return (
+                  <div className="synonym" key={index}>
+                    {syn}
+                  </div>
+                );
+              })}
+            </article>
+          )}
+          {dict.meaning2 && <article>2. &nbsp;{dict.meaning2}</article>}
         </div>
-        <div className="partOfSpeech">
-          <i>{dict.partOfSpeech1}</i>
-        </div>
+        {dict.partOfSpeech1 && (
+          <div className="partOfSpeech">
+            <i>{dict.partOfSpeech1}</i>
+          </div>
+        )}
         <div className="meaningSection">
-          <article>1.&nbsp; {dict.meaning1Verb}</article>
-          <article className="synonymsDiv">
-            <span>Similar:</span>
-            {dict.synonyms1?.map((syn, index) => {
-              return (
-                <div className="synonym" key={index}>
-                  {syn}
-                </div>
-              );
-            })}
-          </article>
-          <article>2.&nbsp; {dict.meaning2Verb}</article>
+          {dict.meaning1Verb && <article>1.&nbsp; {dict.meaning1Verb}</article>}
+          {dict.synonyms1 && (
+            <article className="synonymsDiv">
+              <span>Similar:</span>
+              {dict.synonyms1?.map((syn, index) => {
+                return (
+                  <div className="synonym" key={index}>
+                    {syn}
+                  </div>
+                );
+              })}
+            </article>
+          )}
+          {dict.meaning2Verb && <article>2.&nbsp; {dict.meaning2Verb}</article>}
         </div>
       </section>
     </>
